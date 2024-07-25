64fcfc909e8606d3f5c1b60b78f8c8e50dfbe0d7
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-25 09:32:01 +0200

    Implemented all needed components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import { useState } from 'react'
+import Search from './Search'
 import './App.css'
 
+const allUsers = [
+  'John',
+  'Alex',
+  'George',
+  'Simon',
+  'James'
+]
+
 function App() {
+  const [users, setUsers] = useState(allUsers)
+
+  function handleSearch(text) {
+    console.log(text)
+    const filteredUsers = users.filter(user => user.includes(text))
+    setUsers(filteredUsers)
+  }
   return (
-    <>
-      hello world
-    </>
+    <div className="container">
+      <div className="content">
+        <button onClick={() => setUsers(shuffle(allUsers))}>Shuffle</button>
+
+        <Search onChange={handleSearch} />
+      </div>
+      <ul>
+        {
+          users.map((user, index) => (
+            <li key={index}>{user}</li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
 
